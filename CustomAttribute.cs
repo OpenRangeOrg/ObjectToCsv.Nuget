3b840c10396a5a4d6eb9930f9dce5653bac07224
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2024-02-17 20:27:32 +0100

    Adding code for csv conversion

---
+++ CustomAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace ObjectToCsv
+{
+    public class Header : Attribute
+    {
+        public string HeaderName { get; private set; }
+        public Header(string headerName)
+        {
+            HeaderName = headerName;
+        }
+    }
+    public class Ordinal : Attribute
+    {
+        public int Order { get; private set; }
+        public Ordinal(int order)
+        {
+            Order = order;
+        }
+    }
+}
